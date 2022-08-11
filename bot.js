f91df736b96dc706a8ba34e154608d838620df2e
GainsNetwork <contact@gains.trade>
2022-08-11 11:10:54 +0200

    fetch nftRewards from callbacks instead

--- bot.js
+++ bot.js
@@ -150,22 +150,21 @@ async function setCurrentWeb3Client(newWeb3ClientIndex){
 
 	const aggregatorContract = new newWeb3Client.eth.Contract(abis.AGGREGATOR, aggregatorAddress);
 
-	// Retrieve all necessary details from the aggregator contract
+	callbacksContract = new newWeb3Client.eth.Contract(abis.CALLBACKS, callbacksAddress);
+	tradingContract = new newWeb3Client.eth.Contract(abis.TRADING, tradingAddress);
+	vaultContract = new newWeb3Client.eth.Contract(abis.VAULT, vaultAddress);
+	pairInfosContract = new newWeb3Client.eth.Contract(abis.PAIR_INFOS, process.env.PAIR_INFOS_ADDRESS);
+
 	const [
 		pairsStorageAddress,
 		nftRewardsAddress
 	 ] = await Promise.all([
 		aggregatorContract.methods.pairsStorage().call(),
-		aggregatorContract.methods.nftRewards().call()
+		callbacksContract.methods.nftRewards().call()
 	 ]);
 
 	pairsStorageContract = new newWeb3Client.eth.Contract(abis.PAIRS_STORAGE, pairsStorageAddress);
 	nftRewardsContract = new newWeb3Client.eth.Contract(abis.NFT_REWARDS, nftRewardsAddress);
-
-	callbacksContract = new newWeb3Client.eth.Contract(abis.CALLBACKS, callbacksAddress);
-	tradingContract = new newWeb3Client.eth.Contract(abis.TRADING, tradingAddress);
-	vaultContract = new newWeb3Client.eth.Contract(abis.VAULT, vaultAddress);
-	pairInfosContract = new newWeb3Client.eth.Contract(abis.PAIR_INFOS, process.env.PAIR_INFOS_ADDRESS);
 
 	linkContract = new newWeb3Client.eth.Contract(abis.LINK, linkAddress);
 
